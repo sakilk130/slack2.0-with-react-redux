03e72b9c39b9d796c6a5a75d06047ef7a944e7d4
sakilk130 <sakilk130@gmail.com>
2021-11-29 01:05:30 +0600

    add message input and chat room

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,7 +1,76 @@
 import React from 'react';
+import styled from 'styled-components';
+import { StarBorderOutlined, InfoOutlined } from '@material-ui/icons';
+import ChatInput from './ChatInput/ChatInput';
+import { useSelector } from 'react-redux';
+import { selectRoomId } from '../../features/appSlice';
 
 function Chat() {
-  return <div>Chat</div>;
+  const roomId = useSelector(selectRoomId);
+  return (
+    <ChatContainer>
+      <Header>
+        <HeaderLeft>
+          <h4>
+            <strong>#Room name</strong>
+          </h4>
+          <StarBorderOutlined />
+        </HeaderLeft>
+        <HeaderRight>
+          <p>
+            <InfoOutlined /> Details
+          </p>
+        </HeaderRight>
+      </Header>
+      <ChatMessages></ChatMessages>
+      <ChatInput channelId={roomId} />
+    </ChatContainer>
+  );
 }
 
 export default Chat;
+
+const ChatContainer = styled.div`
+  flex: 0.7;
+  flex-grow: 1;
+  overflow-y: scroll;
+  margin-top: 60px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  border-bottom: 1px solid lightgray;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+  > h4 {
+    display: flex;
+    align-items: center;
+    text-transform: lowercase;
+    margin-right: 10px;
+
+    > h4 .MuiSvgIcon-root {
+      margin-left: 10px;
+      font-size: 18px;
+    }
+  }
+`;
+
+const HeaderRight = styled.div`
+  > p {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  }
+  > p .MuiSvgIcon-root {
+    margin-right: 5px !important;
+    font-size: 16px;
+  }
+`;
+
+const ChatMessages = styled.div``;
---
+++ src/components/Chat/ChatInput/ChatInput.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Button } from '@material-ui/core';
+import db from '../../../firebase/config';
+import firebase from 'firebase';
+
+interface IChatInput {
+  channelName?: string;
+  channelId?: string;
+  chatRef?: any;
+}
+
+function ChatInput({ channelName, channelId, chatRef }: IChatInput) {
+  const [input, setInput] = useState('');
+
+  const sendMessage = (e: any) => {
+    e.preventDefault();
+    if (!channelId) {
+      return false;
+    }
+    db.collection('rooms').doc(channelId).collection('messages').add({
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      user: 'Sakil Khan',
+      userImage: '',
+    });
+  };
+  return (
+    <ChatInputContainer>
+      <form>
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          placeholder={`Message #${channelName}`}
+        />
+        <Button hidden type="submit" onClick={sendMessage}>
+          SEND
+        </Button>
+      </form>
+    </ChatInputContainer>
+  );
+}
+
+export default ChatInput;
+
+const ChatInputContainer = styled.div`
+  border-radius: 20px;
+  > form {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    > input {
+      position: fixed;
+      bottom: 30px;
+      width: 60%;
+      border: 1px solid gray;
+      border-radius: 3px;
+      padding: 20px;
+      outline: none;
+    }
+    > button {
+      display: none !important;
+    }
+  }
+`;
